768cd5b60b5389f480bf9726b6d03afd4a22ba5a
acrylic-style <me@acrylicstyle.xyz>
2022-08-18 01:25:47 +0900

    add intents

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv-safe').config()
 const Discord = require('discord.js')
 const client = new Discord.Client({
-  intents: Discord.Intents.FLAGS.GUILD_MESSAGES | Discord.Intents.FLAGS.GUILDS | Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  intents: Discord.Intents.FLAGS.GUILD_MESSAGES | Discord.Intents.FLAGS.GUILDS | Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS | Discord.Intents.FLAGS.DIRECT_MESSAGES | Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
 })
 const fs = require('fs')
 
